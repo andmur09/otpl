b7d92dce35ec27e79ddfa64846f78df8a0c8b38b
Andrew Murray <kpb20194@pd-michae2.cis.strath.ac.uk>
2022-08-18 11:55:37 +0100

    testing setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='otpl',
+    version='0.0.1',
+    author='Michael Cashmore',
+    author_email='michael.cashmore@strath.ac.uk',
+    description='Open Task Planning Library',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/taskplanning/otpl',
+    project_urls = {
+        "Bug Tracker": "https://github.com/taskplanning/otpl/issues"
+    },
+    license='LICENSE.txt',
+    packages=find_packages(),
+    install_requires=['antlr4-python3-runtime==4.10', 'numpy', 're'],
+)
