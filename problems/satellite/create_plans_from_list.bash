9a3266782f0f6059a7f7a6f0376422364b145a3d
Andrew Murray <kpb20194@pd-michae2.cis.strath.ac.uk>
2022-09-06 14:27:36 +0100

    removed unecessary files

--- problems/satellite/create_plans_from_list.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-path=$(pwd)
-domain=$path/domain.pddl
-input=$path/to_run.txt
-
-while IFS= read -r file; do
-    echo "Solving Problem ${FILE}";
-    timeout 1800 optic-cplex $domain ${path}/instances/${file} > ${path}/results/satellite_${file%.*}_plan.pddl;
-done < "$input"
